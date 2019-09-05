c5fd3088cea5728667a8e5a2a751b9d37c75bd77
Adam Vessey <adam-vessey@users.noreply.github.com>
2019-09-05 15:55:11 -0300

    Redirect node's media creation back to node.

---
+++ src/Form/NodeMediaRedirect.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\basic_ingest\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Helper; redirect to the related node when ingest media belonging to a ndoe.
+ */
+class NodeMediaRedirect {
+
+  const NODE_COORDS = [
+    'field_media_of',
+    0,
+    'target_id',
+  ];
+
+  /**
+   * Delegated hook_form_alter().
+   */
+  public static function alter(array &$form, FormStateInterface $form_state) {
+    $form['actions']['submit']['#submit'][] = [static::class, 'submit'];
+  }
+
+  /**
+   * Form submission handler.
+   */
+  public static function submit(array &$form, FormStateInterface $form_state) {
+    $node_id = $form_state->getValue(static::NODE_COORDS);
+    if ($node_id) {
+      $form_state->setRedirect('entity.node.canonical', [
+        'node' => $node_id,
+      ]);
+    }
+  }
+
+}
